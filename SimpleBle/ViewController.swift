7478e079baec8b264228e155decac69aa20c43e5
Robert Hartman <roberth@spindance.com>
2020-03-05 06:18:33 -0500

    Initial Commit

---
+++ SimpleBle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBle
+//
+//  Created by Robert Hartman on 3/5/20.
+//  Copyright © 2020 SpinDance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
